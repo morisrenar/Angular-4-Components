564bcca66a2a82da7461b5968ddd8c981a2dca9d
bmshamsnahid <morisrenar@gmail.com>
2017-06-16 07:51:19 +0600

    021

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent implements OnInit {
+
+  stringInterpolation = 'This is string interpolation';
+  numberInterpolation = 2;
+
+  constructor() { }
+  
+  ngOnInit() {
+  }
+  
+  onTest() {
+    return true;
+  }
+
+}
